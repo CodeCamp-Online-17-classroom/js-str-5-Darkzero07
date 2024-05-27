61242f56aa7f516e25a7ed185c33d503ccbedb9e
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-27 07:11:03 +0700

    js lab 85

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// write code here
+// truncate("What I'd like to tall on this topic is:", 20); // What I'd like to te...
+// truncate("Hi everyone!", 20); // Hi everyone!
+
+function truncate(str, length) {
+  if (str.length < length) {
+    return str;
+  } else {
+    return str.slice(0, length-1) + "...";
+  }
+}
+
+console.log(truncate("What I'd like to tall on this topic is:", 20)); // What I'd like to te...
+console.log(truncate("Hi everyone!", 20)); // Hi everyone!
